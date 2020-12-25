e64d5e0e732fa0f78547b6bc303815108ffcaea0
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2020-12-25 19:45:11 -0400

    Delete DepartmentsControler and View

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,9 +1,13 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
     }
 }
